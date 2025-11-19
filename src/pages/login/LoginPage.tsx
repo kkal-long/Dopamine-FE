e2d78fd32c162405e93209160d8e1ad4a6491d55
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-19 23:05:23 +0900

    [FEAT] 로그인 페이지 API 연동

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,15 +1,17 @@
 import { Kakao, Logo } from "@/assets/svgs/common";
 
+const KAKAO_AUTH_URL = `${import.meta.env.VITE_SERVER_API_URL}/oauth2/authorization/kakao`;
+
 const LoginPage = () => {
   const handleKakaoLogin = () => {
-    alert("카카오 로그인 페이지 이동");
+    window.location.href = KAKAO_AUTH_URL;
   };
 
   return (
     <div className="relative h-screen">
       <div className="absolute top-[40%] left-1/2 -translate-1/2 w-full flex flex-col items-center px-8">
-        <Logo className="w-20 h-10 mb-5" />
-        <h1 className="text-center text-semibold20 font-semibold text-darkgrey05 mb-14">
+        <Logo className="w-32 h-32" />
+        <h1 className="text-center text-semibold20 text-darkgrey05 mb-14">
           간편하게 로그인하고
           <br />
           바로 서비스를 이용해보세요
