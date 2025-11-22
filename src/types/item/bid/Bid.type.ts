79a505b89bfb923cfaf31a624fb63870fa16a1f6
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-23 00:51:42 +0900

    [FEAT] API 호출 타입 정의

--- src/types/item/bid/Bid.type.ts
+++ src/types/item/bid/Bid.type.ts
@@ -1,31 +1,5 @@
 import type { ComponentType, SVGProps } from "react";
 
-export interface WonItemData {
-  autionId: number;
-  title: string;
-  image: string;
-  wonPrice: number;
-  state: "ENDED";
-  roomId: number;
-}
-
-export interface WonItems {
-  autions: WonItemData[];
-}
-
-export interface BiddingItemData {
-  autionId: number;
-  title: string;
-  image: string;
-  bidPrice: number;
-  state: "RUNNING" | "ENDED";
-  endsAt: string;
-}
-
-export interface BiddingItems {
-  autions: BiddingItemData[];
-}
-
 export type SvgIcon = ComponentType<SVGProps<SVGSVGElement>>;
 
 export type MainPageProduct = {
