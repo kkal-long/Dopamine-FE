2ff33d7dbfee827ebc071a90806aa2365687ccf9
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-23 22:47:56 +0900

    [FEAT] API 타입 정의

--- src/types/chat/chatApi.type.ts
+++ src/types/chat/chatApi.type.ts
@@ -1,3 +1,21 @@
 export interface CreateChatRoomResponse {
   chatRoomId: number;
+  auctionId: number;
+  sellerId: number;
+  sellerNickname: string;
+  buyerId: number;
+  buyerNickname: string;
 }
+
+export interface ChatMessageItem {
+  messageId: number;
+  senderId: number;
+  senderName: string;
+  profileImageUrl: string;
+  messageContent: string;
+  sendAt: string;
+  isRead: boolean;
+  myMessage: boolean;
+}
+
+export type ChatMesageList = ChatMessageItem[];
