9c831ed6224c6654595ebd3db690a14741ac978d
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-25 02:34:19 +0900

    [REFACTOR] 카테고리 정보를 constants 폴더로 이동

--- src/pages/search/CategorySelectPage.tsx
+++ src/pages/search/CategorySelectPage.tsx
@@ -1,35 +1,7 @@
 import { Search } from "@/assets/svgs/search";
+import { categories } from "@/constants/Category";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
-// SVG 아이콘 import (11개 모두 추가, 프론트에서 처리)
-import {
-  Category1,
-  Category10,
-  Category11,
-  Category2,
-  Category3,
-  Category4,
-  Category5,
-  Category6,
-  Category7,
-  Category8,
-  Category9,
-} from "@/assets/svgs/search";
-
-const categories = [
-  { id: 1, name: "디지털 기기", Icon: Category1 },
-  { id: 2, name: "가구/인테리어", Icon: Category2 },
-  { id: 3, name: "유아동", Icon: Category3 },
-  { id: 4, name: "생활가전", Icon: Category4 },
-  { id: 5, name: "스포츠", Icon: Category5 },
-  { id: 6, name: "가공식품", Icon: Category6 },
-  { id: 7, name: "취미/게임/음반", Icon: Category7 },
-  { id: 8, name: "도서", Icon: Category8 },
-  { id: 9, name: "남성패션", Icon: Category9 },
-  { id: 10, name: "여성패션", Icon: Category10 },
-  { id: 11, name: "식물", Icon: Category11 },
-];
 
 const CategorySelectPage: React.FC = () => {
   const navigate = useNavigate();
